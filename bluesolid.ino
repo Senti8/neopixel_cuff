fac631fbfc6757bd33a71e6cecf9084972db7da4
jbzurn <jbzurn@gmail.com>
2014-04-13 02:40:16 -0400

    updated for 6 neopixels

--- bluesolid.ino
+++ bluesolid.ino
@@ -1,4 +1,4 @@
-////bluechase implements a blue solid pattern in 5 neopixels
+////bluechase implements a blue solid pattern in 6 neopixels
 void bluesolid(uint8_t wait) {
   uint16_t i, j;
 
@@ -7,6 +7,7 @@ void bluesolid(uint8_t wait) {
   strip.setPixelColor(2, strip.Color(0, 0, 255));
   strip.setPixelColor(3, strip.Color(0, 0, 255));
   strip.setPixelColor(4, strip.Color(0, 0, 255));
+  strip.setPixelColor(5, strip.Color(0, 0, 255));
   strip.show();
   delay(wait);
  

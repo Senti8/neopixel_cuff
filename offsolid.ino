d297c52c18d165cc5b141f51c25af136077628fc
jbzurn <jbzurn@gmail.com>
2014-04-13 01:39:53 -0400

    Create offsolid.ino

---
+++ offsolid.ino
@@ -0,0 +1,13 @@
+/////offsolid turns 5 neopixels off.
+void offsolid(uint8_t wait) {
+  uint16_t i, j;
+  
+ strip.setPixelColor(0, strip.Color(0, 0, 0));
+  strip.setPixelColor(1, strip.Color(0, 0, 0));
+  strip.setPixelColor(2, strip.Color(0, 0, 0));
+  strip.setPixelColor(3, strip.Color(0, 0, 0));
+  strip.setPixelColor(4, strip.Color(0, 0, 0));
+  strip.show();
+  delay(wait);
+ 
+}
